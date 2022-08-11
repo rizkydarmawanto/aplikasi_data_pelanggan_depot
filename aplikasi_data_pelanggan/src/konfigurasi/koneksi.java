b8bd55d0e4cb496bbeb2a8978290030ed6c18311
Muhammad Rizky Darmawanto <58199777+rizkydarmawanto@users.noreply.github.com>
2022-08-11 21:11:03 +0700

    Add files via upload

---
+++ aplikasi_data_pelanggan/src/konfigurasi/koneksi.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konfigurasi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author user
+ */
+public class koneksi {
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/data_depot";
+        String user = "root";
+        String password = "";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection c = koneksi.getConnection();
+            System.out.println(String.format("Connected to database %s " + "successfully.", c.getCatalog()));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
